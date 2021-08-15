503af904f2ad25ee7338e05dcc07658a7da5f738
Alexis Medina <82667507+alexisnpavlik@users.noreply.github.com>
2021-08-15 19:26:37 -0300

    cambio

--- Python Profesional/Tipado.py
+++ Python Profesional/Tipado.py
@@ -1,26 +1,32 @@
 #Definimos el tipo de datos
 x: int =5
-
 z= str ="hola"
-
 c: bool = True
 
+#Definimos el typado y el output
 def suma(a:int , b:int) -> int: 
     return a+b
 
-print (suma("2","5"))
+print (suma(3,2))
 
-#Estructuras de datos
-from typing import List, dict , list
 
+from typing import dict , list
+
+#Listas
 numeros_positivos:list[int] = [1,2,3,4,5]
-
+#Diccionarios
 usuarios: dict[str , int] = {
     'argentina':1,
     'paraguay':3,
-    'brasil':4
-}
+    'brasil':4}
+#tuplas
+from typing import tuple
 
+number: tuple[int,float,str]=[1,0.5,"s"]
+
+#Estructuras complejas
+
+#Lista de diccionarios
 pobracion: list[dict[str , int]] = [
     {'argentina':1,
     'people':52878},
@@ -29,13 +35,7 @@ pobracion: list[dict[str , int]] = [
     {'brasil':4,
     'people':52878}]
 
-
-#tuplas
-from typing import tuple
-
-number: tuple[int,float,str]=[1,0.5,"s"]
-
-
+#Listas de diccionarios con tuplas con Keys
 coordenadas: list[dict[str, tuple[int,int]]]=[
     {'coor_1': (45,25),
     'coor_2': (85,55)},
