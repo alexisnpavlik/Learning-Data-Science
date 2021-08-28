852f11691d157e0694d5ebe3df59d1bfd72efdb7
Alexis Medina <82667507+alexisnpavlik@users.noreply.github.com>
2021-08-28 19:23:36 -0300

    add

---
+++ Metodos Numericos/Interpolacion.py
@@ -0,0 +1,21 @@
+from scipy.interpolate import lagrange
+import matplotlib.pyplot as plt
+import pandas as pd
+
+x=[390,400,410]
+y=[1.548,1.63,1.51]
+
+plt.plot(x,y, 'o')
+plt.xlabel('x')
+plt.ylabel('y')
+
+p=lagrange(x,y)
+print("El polinomio sera:")
+
+
+
+print(p)
+print("valor en x")
+print(p(2))
+
+plt.show()
