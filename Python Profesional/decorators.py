6c8667a4e5be16e871b067a53236e0018ff9595d
Alexis Medina <alexisnpavlik@gmail.com>
2021-10-28 18:05:11 -0300

    Update decorators.py
    Algunos cambios ligeros

--- Python Profesional/decorators.py
+++ Python Profesional/decorators.py
@@ -4,14 +4,14 @@ def time_execute(func):
     def timer():
         inicial_time=datetime.now()
         func()
-        absolute_time=f"Pasaron {(datetime.now()-inicial_time).total_seconds()} segundos"
-        return absolute_time
+        relative_time=f"Pasaron {(datetime.now()-inicial_time).total_seconds()} segundos"
+        return relative_time
     return timer
 
 @time_execute
 def suma():
     for i in range(1,100):
         print(i+i**2)
-
-print(suma())
+        
+print(f"El tiempo de ejcuccion fue de: {suma()} segundos")
 
